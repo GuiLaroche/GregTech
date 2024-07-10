31cdba13d9df61ea6f5744f28c3e0f2f3cde4c76
GuiLaroche <gular105@ulaval.ca>
2024-07-09 23:22:11 -0400

    create gregification part 2 + new items

--- kubejs/server_scripts/create/block_craft.js
+++ kubejs/server_scripts/create/block_craft.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes((event) => {
 
-    event.remove({ id: `create:hand_crank` });
+    event.remove({ id: `create:crafting/kinetics/hand_crank` });
     event.shaped('create:hand_crank', ["LLL", "AsS"], {
         L: '#minecraft:logs',
         A: "minecraft:air",
@@ -8,16 +8,28 @@ ServerEvents.recipes((event) => {
         S: 'create:shaft'
       });
 
-      event.remove({ id: 'create:water_wheel' });
-      event.shaped('create:water_wheel', ["ALA", "LSL", "LWL"], {
+      event.remove({ id: `create:crafting/kinetics/water_wheel` });
+      event.shaped('create:water_wheel', ["ALA", "LSL", "ALA"], {
           L: '#minecraft:logs',
           A: '#forge:ingots/andesite_alloy',
           S: 'create:shaft',
         });
 
-      event.remove({ id: 'create:andesite_alloy' });
+      event.remove({ id: `create:crafting/materials/andesite_alloy` });
+      event.remove({ id: `create:crafting/materials/andesite_alloy_from_zinc` });
       event.shaped('create:andesite_alloy', ["AW", "WA"], {
           W: '#forge:nuggets/wrought_iron',
           A: 'minecraft:andesite',
         });
+
+      event.remove({ id: `create:crafting/kinetics/mechanical_mixer` });
+      event.shaped('create:mechanical_mixer', ["ACA", "tBs", "SWs"], {
+            A: 'minecraft:air',
+            C: 'create:cogwheel',
+            t: '#forge:tools/screwdrivers',
+            B: 'create:andesite_casing',
+            s: '#forge:tools/saws',
+            S: 'gtceu:wrought_iron_screw',
+            W: 'create:whisk'
+          });
   });
--- kubejs/startup_scripts/material_testing/material.js
+++ kubejs/startup_scripts/material_testing/material.js
@@ -16,3 +16,12 @@ GTCEuStartupEvents.registry("gtceu:material", (event) => {
     .flags(GTMaterialFlags.GENERATE_PLATE);
     */
 });
+
+GTCEuStartupEvents.registry('gtceu:material', event => {
+
+  GTMaterials.get('andesite_alloy').addFlags(GTMaterialFlags.GENERATE_PLATE);
+  GTMaterials.get('andesite_alloy').addFlags(GTMaterialFlags.GENERATE_ROD);
+  GTMaterials.get('andesite_alloy').addFlags(GTMaterialFlags.GENERATE_SPRING);
+  GTMaterials.get('andesite_alloy').addFlags(GTMaterialFlags.GENERATE_WIRE);
+  GTMaterials.get('andesite_alloy').addFlags(GTMaterialFlags.GENERATE_SPRING_SMALL);
+});
