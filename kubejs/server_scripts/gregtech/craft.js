bc9ae7a797d7d384a05e4fa8c2734760361bf59b
GuiLaroche <gular105@ulaval.ca>
2024-07-17 00:26:22 -0400

    gregtech config changes and few new recipes

--- kubejs/server_scripts/gregtech/craft.js
+++ kubejs/server_scripts/gregtech/craft.js
@@ -3,4 +3,17 @@ ServerEvents.recipes((event) => {
     event.remove({ id: `gtceu:smelting/dust_bronze__demagnetize_from_dust` });
     event.remove({ id: `gtceu:smelting/dust_brass__demagnetize_from_dust` });
     event.remove({ id: `gtceu:smelting/dust_invar__demagnetize_from_dust` });
+
+
+    event.shaped(`thermal:sawdust`, ["SSS", "SmS", "SSS"], {
+      S: '#forge:bolts/wood',
+      m: '#forge:tools/mortars',
+    });
+
+    event.remove({ id: `gtceu:shaped/mortar_flint` });
+    event.shaped(`gtceu:flint_mortar`, ["AFA", "SFS", "SSS"], {
+      S: '#forge:cobblestone/normal',
+      A: 'minecraft:air',
+      F: '#forge:gems/flint',
+    });
   });
