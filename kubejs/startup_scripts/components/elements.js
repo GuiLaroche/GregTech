1db30ef651fbc1a3cb73dde1a9cc80a8fad00720
Guillaume Laroche <guillaume.laroche.5@ulaval.ca>
2024-07-26 13:02:05 -0400

    added new elements

--- kubejs/startup_scripts/components/elements.js
+++ kubejs/startup_scripts/components/elements.js
@@ -9,7 +9,18 @@ GTCEuStartupEvents.registry('gtceu:element', event => {
  GTCEuStartupEvents.registry('gtceu:element', event => {
     event.create('coaxium', 130, 196, -1, null, 'Cx', false)
  })
- 
+
  GTCEuStartupEvents.registry('gtceu:element', event => {
     event.create('vibranium', 131, 200, -1, null, 'Vb', false)
+ })
+
+ GTCEuStartupEvents.registry('gtceu:element', event => {
+    event.create('necrodermis', 132, 201, -1, null, 'Nc', false)
+ })
+// Feywild element
+ GTCEuStartupEvents.registry('gtceu:element', event => {
+    event.create('sylvanium', 125, 180, -1, null, 'Sy', false)
+ })
+ GTCEuStartupEvents.registry('gtceu:element', event => {
+    event.create('glamorite', 124, 110, -1, null, 'Gl', false)
  })
