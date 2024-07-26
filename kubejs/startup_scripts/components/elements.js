66db614239f5f7a2626476d89e8781ed98d9d41e
Guillaume Laroche <guillaume.laroche.5@ulaval.ca>
2024-07-25 22:36:58 -0400

    added new elements + reorganisation of files

---
+++ kubejs/startup_scripts/components/elements.js
@@ -0,0 +1,15 @@
+GTCEuStartupEvents.registry('gtceu:element', event => {
+    event.create('adamantite', 128, 189, -1, null, 'Ad', false)
+ })
+
+ GTCEuStartupEvents.registry('gtceu:element', event => {
+    event.create('mythril', 126, 151, -1, null, 'My', false)
+ })
+
+ GTCEuStartupEvents.registry('gtceu:element', event => {
+    event.create('coaxium', 130, 196, -1, null, 'Cx', false)
+ })
+ 
+ GTCEuStartupEvents.registry('gtceu:element', event => {
+    event.create('vibranium', 131, 200, -1, null, 'Vb', false)
+ })
